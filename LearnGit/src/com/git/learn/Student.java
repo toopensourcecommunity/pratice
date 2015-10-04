be4c000bbc67ffd5a0c2e826ee506aaa7fe66e33
dummy user <dummyuser@gmail.com>
2015-10-04 12:04:24 -0700

    Added equals method to Student class

--- LearnGit/src/com/git/learn/Student.java
+++ LearnGit/src/com/git/learn/Student.java
@@ -33,4 +33,28 @@ public class Student {
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+	
 }
